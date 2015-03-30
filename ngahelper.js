c96af64d19df023c53ede6cee8d49e86471dbf5b
Icyblade Dai <icyblade.py@gmail.com>
2015-03-30 12:26:36 +0800

    要分号的语言都是异端

--- ngahelper.js
+++ ngahelper.js
@@ -11,22 +11,22 @@
 // ==/UserScript==
 
 // 干掉兽人图片
-document.getElementById("mainmenu").style.margin="0px"
-document.getElementById("custombg").remove()
+document.getElementById("mainmenu").style.margin="0px";
+document.getElementById("custombg").remove();
 
 // 版头高度增高
-document.getElementById("toppedtopic").style.height="320px"
+document.getElementById("toppedtopic").style.height="320px";
 
 // 干掉底部版权信息
-document.getElementsByClassName("cpinfo")[0].remove()
+document.getElementsByClassName("cpinfo")[0].remove();
 
 // 干掉底部快速发帖
-document.getElementById("fast_post_c").remove()
-var count = document.getElementsByTagName("a").length
-document.getElementsByTagName("a")[count-1].remove()
+document.getElementById("fast_post_c").remove();
+var count = document.getElementsByTagName("a").length;
+document.getElementsByTagName("a")[count-1].remove();
 
 // 干掉审核字样
-var red = document.querySelectorAll("[title=\"待审核\"]")
+var red = document.querySelectorAll("[title=\"待审核\"]");
 for(i = 0; i < red.length; i++) {
-	red[i].parentNode.removeChild(red[i])
+	red[i].parentNode.removeChild(red[i]);
 }
